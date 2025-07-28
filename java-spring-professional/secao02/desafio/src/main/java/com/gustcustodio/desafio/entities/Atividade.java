f77b8c7aa66969770f6d539a27b659d31acd1fa1
gustcustodio <gustcustodio@gmail.com>
2025-07-28 06:22:11 -0300

    feat: criando entidade Atividade e relacionamento entre as classes

--- java-spring-professional/secao02/desafio/src/main/java/com/gustcustodio/desafio/entities/Atividade.java
+++ java-spring-professional/secao02/desafio/src/main/java/com/gustcustodio/desafio/entities/Atividade.java
@@ -2,30 +2,35 @@ package com.gustcustodio.desafio.entities;
 
 import jakarta.persistence.*;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Objects;
 
 @Entity
-@Table(name = "tb_categoria")
-public class Categoria {
+@Table(name = "tb_atividade")
+public class Atividade {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
+    private String nome;
+
     @Column(columnDefinition = "TEXT")
     private String descricao;
 
-    @OneToMany(mappedBy = "categoria")
-    private List<Atividade> atividades = new ArrayList<>();
+    private Double preco;
 
-    public Categoria() {
+    @ManyToOne
+    @JoinColumn(name = "categoria_id")
+    private Categoria categoria;
+
+    public Atividade() {
     }
 
-    public Categoria(Integer id, String descricao) {
+    public Atividade(Integer id, String nome, String descricao, Double preco) {
         this.id = id;
+        this.nome = nome;
         this.descricao = descricao;
+        this.preco = preco;
     }
 
     public Integer getId() {
@@ -36,6 +41,14 @@ public class Categoria {
         this.id = id;
     }
 
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
     public String getDescricao() {
         return descricao;
     }
@@ -44,13 +57,21 @@ public class Categoria {
         this.descricao = descricao;
     }
 
+    public Double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(Double preco) {
+        this.preco = preco;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (o == null || getClass() != o.getClass()) return false;
 
-        Categoria categoria = (Categoria) o;
+        Atividade atividade = (Atividade) o;
 
-        return Objects.equals(id, categoria.id);
+        return Objects.equals(id, atividade.id);
     }
 
     @Override
