5cbfee3e6614f9f7bd52869a444d19abda5f4b10
gustcustodio <gustcustodio@gmail.com>
2025-07-31 12:18:14 -0300

    feat: busca paginada de produtos

--- java-spring-professional/dscommerce/src/main/java/com/gustcustodio/dscommerce/services/ProductService.java
+++ java-spring-professional/dscommerce/src/main/java/com/gustcustodio/dscommerce/services/ProductService.java
@@ -4,7 +4,10 @@ import com.gustcustodio.dscommerce.dto.ProductDTO;
 import com.gustcustodio.dscommerce.entities.Product;
 import com.gustcustodio.dscommerce.repositories.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
@@ -14,9 +17,16 @@ public class ProductService {
     @Autowired
     private ProductRepository repository;
 
+    @Transactional(readOnly = true)
     public ProductDTO findById(Long id) {
         Optional<Product> result = repository.findById(id);
         Product product = result.get();
         return new ProductDTO(product);
+    }
+
+    @Transactional(readOnly = true)
+    public Page<ProductDTO> findAll(Pageable pageable) {
+        Page<Product> result = repository.findAll(pageable);
+        return result.map(ProductDTO::new);
     }
 }
