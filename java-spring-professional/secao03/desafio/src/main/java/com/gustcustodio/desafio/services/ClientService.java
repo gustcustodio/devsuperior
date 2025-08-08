ff9f58447425069d508356b58d7bcf383756450d
gustcustodio <gustcustodio@gmail.com>
2025-08-08 06:45:56 -0300

    feat: busca paginada de clients

--- java-spring-professional/secao03/desafio/src/main/java/com/gustcustodio/desafio/services/ClientService.java
+++ java-spring-professional/secao03/desafio/src/main/java/com/gustcustodio/desafio/services/ClientService.java
@@ -4,6 +4,8 @@ import com.gustcustodio.desafio.dto.ClientDTO;
 import com.gustcustodio.desafio.entities.Client;
 import com.gustcustodio.desafio.repositories.ClientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -18,6 +20,11 @@ public class ClientService {
         Optional<Client> optionalClient = clientRepository.findById(id);
         Client client = optionalClient.get();
         return new ClientDTO(client);
+    }
+
+    public Page<ClientDTO> findAll(Pageable pageable) {
+        Page<Client> clientPage = clientRepository.findAll(pageable);
+        return clientPage.map(ClientDTO::new);
     }
 
 }
