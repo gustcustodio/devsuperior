1003a446cef63a48d9ac02f658b4803e3b6e140d
gustcustodio <gustcustodio@gmail.com>
2025-08-29 06:28:12 -0300

    feat: URI 2737 Spring Boot SQL

--- java-spring-professional/secao04/bdsconsultas-main/uri2737/src/main/java/com/devsuperior/uri2737/Uri2737Application.java
+++ java-spring-professional/secao04/bdsconsultas-main/uri2737/src/main/java/com/devsuperior/uri2737/Uri2737Application.java
@@ -1,5 +1,7 @@
 package com.devsuperior.uri2737;
 
+import com.devsuperior.uri2737.dto.LawyerMinDTO;
+import com.devsuperior.uri2737.projections.LawyerMinProjection;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -7,19 +9,29 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.devsuperior.uri2737.repositories.LawyerRepository;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @SpringBootApplication
 public class Uri2737Application implements CommandLineRunner {
 
-	@Autowired
-	private LawyerRepository repository;
-	
-	public static void main(String[] args) {
-		SpringApplication.run(Uri2737Application.class, args);
-	}
+    @Autowired
+    private LawyerRepository repository;
 
-	@Override
-	public void run(String... args) throws Exception {
-		
+    public static void main(String[] args) {
+        SpringApplication.run(Uri2737Application.class, args);
+    }
 
-	}
+    @Override
+    public void run(String... args) throws Exception {
+
+        List<LawyerMinProjection> list1 = repository.search1();
+        List<LawyerMinDTO> result = list1.stream().map(LawyerMinDTO::new).collect(Collectors.toList());
+
+        System.out.println();
+        for (LawyerMinDTO obj : result) {
+            System.out.println(obj);
+        }
+
+    }
 }
