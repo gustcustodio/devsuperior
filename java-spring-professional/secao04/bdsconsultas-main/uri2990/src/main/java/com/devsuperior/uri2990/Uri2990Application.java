8c08dd23cdc15c09cc4758ff022a67a59f29935c
gustcustodio <gustcustodio@gmail.com>
2025-08-30 06:56:47 -0300

    feat: URI 2990 Spring Boot SQL e JPQL

--- java-spring-professional/secao04/bdsconsultas-main/uri2990/src/main/java/com/devsuperior/uri2990/Uri2990Application.java
+++ java-spring-professional/secao04/bdsconsultas-main/uri2990/src/main/java/com/devsuperior/uri2990/Uri2990Application.java
@@ -1,5 +1,7 @@
 package com.devsuperior.uri2990;
 
+import com.devsuperior.uri2990.dto.EmpregadoDeptDTO;
+import com.devsuperior.uri2990.projections.EmpregadoDeptProjection;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -7,18 +9,36 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.devsuperior.uri2990.repositories.EmpregadoRepository;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @SpringBootApplication
 public class Uri2990Application implements CommandLineRunner {
 
-	@Autowired
-	private EmpregadoRepository repository;
-	
-	public static void main(String[] args) {
-		SpringApplication.run(Uri2990Application.class, args);
-	}
+    @Autowired
+    private EmpregadoRepository repository;
 
-	@Override
-	public void run(String... args) throws Exception {
-		
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Uri2990Application.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        List<EmpregadoDeptProjection> list1 = repository.search1();
+        List<EmpregadoDeptDTO> result1 = list1.stream().map(EmpregadoDeptDTO::new).collect(Collectors.toList());
+
+        System.out.println("\n*** SQL RAIZ ***");
+        for (EmpregadoDeptDTO obj : result1) {
+            System.out.println(obj);
+        }
+
+        List<EmpregadoDeptDTO> result2 = repository.search2();
+
+        System.out.println("\n*** JPQL ***");
+        for (EmpregadoDeptDTO obj : result2) {
+            System.out.println(obj);
+        }
+
+    }
 }
