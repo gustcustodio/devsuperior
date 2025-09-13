a3c323ca5266f4df4ee5b5ce860e4b70ac693faa
gustcustodio <gustcustodio@gmail.com>
2025-09-13 07:00:27 -0300

    feat: obtendo o usuário logado

--- java-spring-professional/dscommerce/src/main/java/com/gustcustodio/dscommerce/services/UserService.java
+++ java-spring-professional/dscommerce/src/main/java/com/gustcustodio/dscommerce/services/UserService.java
@@ -1,14 +1,19 @@
 package com.gustcustodio.dscommerce.services;
 
+import com.gustcustodio.dscommerce.dto.UserDTO;
 import com.gustcustodio.dscommerce.entities.Role;
 import com.gustcustodio.dscommerce.entities.User;
 import com.gustcustodio.dscommerce.projections.UserDetailsProjection;
 import com.gustcustodio.dscommerce.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -35,6 +40,24 @@ public class UserService implements UserDetailsService {
         }
 
         return user;
+    }
+
+    protected User authenticated() {
+
+        try {
+            Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+            Jwt jwtPrincipal = (Jwt) authentication.getPrincipal();
+            String username = jwtPrincipal.getClaim("username");
+            return repository.findByEmail(username).get();
+        } catch (Exception e) {
+            throw new UsernameNotFoundException("Username not found!");
+        }
+
+    }
+
+    @Transactional(readOnly = true)
+    public UserDTO getMe() {
+        return new UserDTO(authenticated());
     }
 
 }
