025f74c755b66f4fb408904c2498136964867a3a
gustcustodio <gustcustodio@gmail.com>
2025-10-30 06:11:59 -0300

    refactor: mudando o nome da exceção

--- java-spring-expert/dscatalog/src/main/java/com/gustcustodio/dscatalog/resources/exceptions/ResourceExceptionHandler.java
+++ java-spring-expert/dscatalog/src/main/java/com/gustcustodio/dscatalog/resources/exceptions/ResourceExceptionHandler.java
@@ -1,6 +1,6 @@
 package com.gustcustodio.dscatalog.resources.exceptions;
 
-import com.gustcustodio.dscatalog.services.exceptions.EntityNotFoundException;
+import com.gustcustodio.dscatalog.services.exceptions.ResourceNotFoundException;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,8 +12,8 @@ import java.time.Instant;
 @ControllerAdvice
 public class ResourceExceptionHandler {
 
-    @ExceptionHandler(EntityNotFoundException.class)
-    public ResponseEntity<StandardError> entityNotFound(EntityNotFoundException e, HttpServletRequest request) {
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<StandardError> entityNotFound(ResourceNotFoundException e, HttpServletRequest request) {
         HttpStatus httpStatus = HttpStatus.NOT_FOUND;
         StandardError standardError =
                 new StandardError(Instant.now(), httpStatus.value(), e.getMessage(), "Entity not found", request.getRequestURI());
--- java-spring-expert/dscatalog/src/main/java/com/gustcustodio/dscatalog/services/CategoryService.java
+++ java-spring-expert/dscatalog/src/main/java/com/gustcustodio/dscatalog/services/CategoryService.java
@@ -3,7 +3,7 @@ package com.gustcustodio.dscatalog.services;
 import com.gustcustodio.dscatalog.dtos.CategoryDTO;
 import com.gustcustodio.dscatalog.entities.Category;
 import com.gustcustodio.dscatalog.repositories.CategoryRepository;
-import com.gustcustodio.dscatalog.services.exceptions.EntityNotFoundException;
+import com.gustcustodio.dscatalog.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -24,7 +24,7 @@ public class CategoryService {
 
     @Transactional(readOnly = true)
     public CategoryDTO findById(Long id) {
-        Category category = categoryRepository.findById(id).orElseThrow(() -> new EntityNotFoundException("Resource not found"));
+        Category category = categoryRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Resource not found"));
         return new CategoryDTO(category);
     }
 
