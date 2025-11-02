c8f845c07eeda71fd91e1a77c6d549c1a92636b4
gustcustodio <gustcustodio@gmail.com>
2025-11-02 09:32:57 -0300

    feat: classe ProductDTO

--- java-spring-expert/dscatalog/src/main/java/com/gustcustodio/dscatalog/dtos/ProductDTO.java
+++ java-spring-expert/dscatalog/src/main/java/com/gustcustodio/dscatalog/dtos/ProductDTO.java
@@ -1,50 +1,48 @@
-package com.gustcustodio.dscatalog.entities;
+package com.gustcustodio.dscatalog.dtos;
 
-import jakarta.persistence.*;
+import com.gustcustodio.dscatalog.entities.Category;
+import com.gustcustodio.dscatalog.entities.Product;
 
 import java.time.Instant;
-import java.util.HashSet;
-import java.util.Objects;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Set;
 
-@Entity
-@Table(name = "tb_product")
-public class Product {
+public class ProductDTO {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     private String name;
-
-    @Column(columnDefinition = "TEXT")
     private String description;
-
     private Double price;
-
     private String imgUrl;
-
-    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
     private Instant date;
 
-    @ManyToMany
-    @JoinTable(
-            name = "tb_product_category",
-            joinColumns = @JoinColumn(name = "product_id"),
-            inverseJoinColumns = @JoinColumn(name = "category_id")
-    )
-    Set<Category> categories = new HashSet<>();
+    private List<CategoryDTO> categories = new ArrayList<>();
 
-    public Product() {
+    public ProductDTO() {
     }
 
-    public Product(Long id, String name, String description, Double price, String imgUrl, Instant date) {
+    public ProductDTO(Long id, String name, String description, Double price, String imgUrl, Instant date) {
         this.id = id;
         this.name = name;
         this.description = description;
         this.price = price;
         this.imgUrl = imgUrl;
         this.date = date;
+    }
+
+    public ProductDTO(Product entity) {
+        this.id = entity.getId();
+        this.name = entity.getName();
+        this.description = entity.getDescription();
+        this.price = entity.getPrice();
+        this.imgUrl = entity.getImgUrl();
+        this.date = entity.getDate();
+    }
+
+    public ProductDTO(Product entity, Set<Category> categories) {
+        this(entity);
+        categories.forEach(category -> this.categories.add(new CategoryDTO(category)));
     }
 
     public Long getId() {
@@ -95,21 +93,8 @@ public class Product {
         this.date = date;
     }
 
-    public Set<Category> getCategories() {
+    public List<CategoryDTO> getCategories() {
         return categories;
     }
-
-    @Override
-    public boolean equals(Object o) {
-        if (o == null || getClass() != o.getClass()) return false;
-
-        Product product = (Product) o;
-        return Objects.equals(id, product.id);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hashCode(id);
-    }
-
+    
 }
