a5d695b367b0da3e14c62c993d0e513b813954cd
gustcustodio <gustcustodio@gmail.com>
2025-11-09 12:41:43 -0300

    feat: fixtures no JUnit, BeforeEach

--- java-spring-expert/dscatalog/src/test/java/com/gustcustodio/dscatalog/repositories/ProductRepositoryTests.java
+++ java-spring-expert/dscatalog/src/test/java/com/gustcustodio/dscatalog/repositories/ProductRepositoryTests.java
@@ -2,6 +2,7 @@ package com.gustcustodio.dscatalog.repositories;
 
 import com.gustcustodio.dscatalog.entities.Product;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -14,9 +15,16 @@ public class ProductRepositoryTests {
     @Autowired
     private ProductRepository productRepository;
 
+    private long existingId;
+
+    @BeforeEach
+    void setUp() throws Exception {
+        existingId = 1L;
+    }
+
     @Test
     public void deleteShouldDeleteObjectWhenIdExists() {
-        long existingId = 1L;
+        existingId = 1L;
         productRepository.deleteById(existingId);
         Optional<Product> result = productRepository.findById(existingId);
         Assertions.assertFalse(result.isPresent());
