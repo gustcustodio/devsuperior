4733c6d292d9610979715a1c419c24d9e7a0141a
gustcustodio <gustcustodio@gmail.com>
2025-11-15 12:10:35 -0300

    feat: teste delete lança DatabaseException quando id dependente

--- java-spring-expert/dscatalog/src/test/java/com/gustcustodio/dscatalog/services/ProductServiceTests.java
+++ java-spring-expert/dscatalog/src/test/java/com/gustcustodio/dscatalog/services/ProductServiceTests.java
@@ -1,6 +1,7 @@
 package com.gustcustodio.dscatalog.services;
 
 import com.gustcustodio.dscatalog.repositories.ProductRepository;
+import com.gustcustodio.dscatalog.services.exceptions.DatabaseException;
 import com.gustcustodio.dscatalog.services.exceptions.ResourceNotFoundException;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
@@ -9,6 +10,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 @ExtendWith(SpringExtension.class)
@@ -22,16 +24,20 @@ public class ProductServiceTests {
 
     private long existingId;
     private long nonExistingId;
+    private long dependentId;
 
     @BeforeEach
     void setUp() throws Exception {
         existingId = 1L;
         nonExistingId = 2L;
+        dependentId = 3L;
 
         Mockito.doNothing().when(productRepository).deleteById(existingId);
+        Mockito.doThrow(DataIntegrityViolationException.class).when(productRepository).deleteById(dependentId);
 
         Mockito.when(productRepository.existsById(existingId)).thenReturn(true);
         Mockito.when(productRepository.existsById(nonExistingId)).thenReturn(false);
+        Mockito.when(productRepository.existsById(dependentId)).thenReturn(true);
     }
 
     @Test
@@ -46,6 +52,13 @@ public class ProductServiceTests {
     public void deleteShouldThrowResourceNotFoundExceptionWhenIdDoesNotExists() {
         Assertions.assertThrows(ResourceNotFoundException.class, () -> {
             productService.delete(nonExistingId);
+        });
+    }
+
+    @Test
+    public void deleteShouldThrowDatabaseExceptionWhenDependentId() {
+        Assertions.assertThrows(DatabaseException.class, () -> {
+            productService.delete(dependentId);
         });
     }
 
