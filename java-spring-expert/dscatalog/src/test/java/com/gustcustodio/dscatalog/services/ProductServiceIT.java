1d6f258f45922dc1ac5baab07c72912266b8dad2
gustcustodio <gustcustodio@gmail.com>
2025-11-18 06:52:41 -0300

    feat: primeiro teste de integração

---
+++ java-spring-expert/dscatalog/src/test/java/com/gustcustodio/dscatalog/services/ProductServiceIT.java
@@ -0,0 +1,42 @@
+package com.gustcustodio.dscatalog.services;
+
+import com.gustcustodio.dscatalog.repositories.ProductRepository;
+import com.gustcustodio.dscatalog.services.exceptions.ResourceNotFoundException;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ProductServiceIT {
+
+    @Autowired
+    private ProductService productService;
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    private Long existingId;
+    private Long nonExistingId;
+    private Long countTotalProducts;
+
+    @BeforeEach
+    void setUp() throws Exception {
+        existingId = 1L;
+        nonExistingId = 1000L;
+        countTotalProducts = productRepository.count();
+    }
+
+    @Test
+    public void deleteShouldDeleteResourceWhenIdExists() {
+        productService.delete(existingId);
+        Assertions.assertEquals(countTotalProducts - 1, productRepository.count());
+    }
+
+    @Test
+    public void deleteShouldThrowResourceNotFoundExceptionWhenIdDoesNotExists() {
+        Assertions.assertThrows(ResourceNotFoundException.class, () -> productService.delete(nonExistingId));
+    }
+
+}
