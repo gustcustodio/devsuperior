a9687b7d2fdfd1e929eecc2985a55f5e9f654fb1
gustcustodio <gustcustodio@gmail.com>
2025-11-26 06:24:42 -0300

    refactor: config para spring security e h2 console

--- java-spring-expert/dscatalog/src/main/java/com/gustcustodio/dscatalog/configs/SecurityConfig.java
+++ java-spring-expert/dscatalog/src/main/java/com/gustcustodio/dscatalog/configs/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 public class SecurityConfig {
@@ -11,7 +12,11 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.csrf(csrf -> csrf.disable());
-        http.authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
+        http.authorizeHttpRequests(auth -> {
+            auth.requestMatchers(AntPathRequestMatcher.antMatcher("/h2-console/**")).permitAll();
+            auth.anyRequest().permitAll();
+        });
+        http.headers(headers -> headers.frameOptions(frameOptions -> frameOptions.disable()));
         return http.build();
     }
 
