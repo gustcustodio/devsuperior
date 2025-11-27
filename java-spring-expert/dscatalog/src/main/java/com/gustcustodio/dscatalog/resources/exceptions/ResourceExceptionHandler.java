cab88f3e62e890b78999a42a77a8122c05c3a12e
gustcustodio <gustcustodio@gmail.com>
2025-11-27 06:33:53 -0300

    feat: resposta customizada para erro de validação

--- java-spring-expert/dscatalog/src/main/java/com/gustcustodio/dscatalog/resources/exceptions/ResourceExceptionHandler.java
+++ java-spring-expert/dscatalog/src/main/java/com/gustcustodio/dscatalog/resources/exceptions/ResourceExceptionHandler.java
@@ -5,6 +5,7 @@ import com.gustcustodio.dscatalog.services.exceptions.ResourceNotFoundException;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -31,11 +32,14 @@ public class ResourceExceptionHandler {
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<StandardError> validation(MethodArgumentNotValidException e, HttpServletRequest request) {
+    public ResponseEntity<ValidationError> validation(MethodArgumentNotValidException e, HttpServletRequest request) {
         HttpStatus httpStatus = HttpStatus.UNPROCESSABLE_ENTITY;
-        StandardError standardError =
-                new StandardError(Instant.now(), httpStatus.value(), e.getMessage(), "Validation exception", request.getRequestURI());
-        return ResponseEntity.status(httpStatus).body(standardError);
+        ValidationError validationError =
+                new ValidationError(Instant.now(), httpStatus.value(), e.getMessage(), "Validation exception", request.getRequestURI());
+        for (FieldError fieldError : e.getBindingResult().getFieldErrors()) {
+            validationError.addError(fieldError.getField(), fieldError.getDefaultMessage());
+        }
+        return ResponseEntity.status(httpStatus).body(validationError);
     }
 
 }
