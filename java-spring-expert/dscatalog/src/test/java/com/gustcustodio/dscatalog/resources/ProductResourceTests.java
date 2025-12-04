c0a7dfdb65af594949417704f08a831a5acab557
gustcustodio <gustcustodio@gmail.com>
2025-12-04 06:50:04 -0300

    feat: fazendo os testes de camada web passarem

--- java-spring-expert/dscatalog/src/test/java/com/gustcustodio/dscatalog/resources/ProductResourceTests.java
+++ java-spring-expert/dscatalog/src/test/java/com/gustcustodio/dscatalog/resources/ProductResourceTests.java
@@ -11,6 +11,7 @@ import org.junit.jupiter.api.Test;
 import org.mockito.ArgumentMatchers;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.http.MediaType;
@@ -22,7 +23,7 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import java.util.List;
 
-@WebMvcTest(ProductResource.class)
+@WebMvcTest(value = ProductResource.class, excludeAutoConfiguration = {SecurityAutoConfiguration.class})
 public class ProductResourceTests {
 
     @Autowired
