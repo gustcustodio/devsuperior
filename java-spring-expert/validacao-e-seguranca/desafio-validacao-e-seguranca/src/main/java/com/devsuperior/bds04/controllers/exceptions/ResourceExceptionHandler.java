8db6c9f1538b4ba8d71e4e8a2f0dfef0e0a23c47
gustcustodio <gustcustodio@gmail.com>
2025-12-10 10:31:40 -0300

    feat: implementação do handler e classes para criação de mensagens de erros personalizadas

---
+++ java-spring-expert/validacao-e-seguranca/desafio-validacao-e-seguranca/src/main/java/com/devsuperior/bds04/controllers/exceptions/ResourceExceptionHandler.java
@@ -0,0 +1,27 @@
+package com.devsuperior.bds04.controllers.exceptions;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.time.Instant;
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ValidationError> validation(MethodArgumentNotValidException e, HttpServletRequest request) {
+        HttpStatus httpStatus = HttpStatus.UNPROCESSABLE_ENTITY;
+        ValidationError validationError =
+                new ValidationError(Instant.now(), httpStatus.value(), e.getMessage(), "Validation exception", request.getRequestURI());
+        for (FieldError fieldError : e.getBindingResult().getFieldErrors()) {
+            validationError.addError(fieldError.getField(), fieldError.getDefaultMessage());
+        }
+        return ResponseEntity.status(httpStatus).body(validationError);
+    }
+
+}
