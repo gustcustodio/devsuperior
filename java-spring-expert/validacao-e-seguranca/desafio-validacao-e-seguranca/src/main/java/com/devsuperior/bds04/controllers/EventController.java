04056e76b353d5d567b8e58081a8dcbe1ef2952b
gustcustodio <gustcustodio@gmail.com>
2025-12-11 06:31:53 -0300

    feat: implementação do Repository, Service e Controller da entidade Event

---
+++ java-spring-expert/validacao-e-seguranca/desafio-validacao-e-seguranca/src/main/java/com/devsuperior/bds04/controllers/EventController.java
@@ -0,0 +1,37 @@
+package com.devsuperior.bds04.controllers;
+
+import com.devsuperior.bds04.dto.EventDTO;
+import com.devsuperior.bds04.services.EventService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+
+@RestController
+@RequestMapping(value = "/events")
+public class EventController {
+
+    @Autowired
+    private EventService eventService;
+
+    @GetMapping
+    public ResponseEntity<Page<EventDTO>> findAll(Pageable pageable) {
+        Page<EventDTO> page = eventService.findAll(pageable);
+        return ResponseEntity.ok().body(page);
+    }
+
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_CLIENT')")
+    @PostMapping
+    public ResponseEntity<EventDTO> insert(@Valid @RequestBody EventDTO dto) {
+        dto = eventService.insert(dto);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("{/id}").buildAndExpand(dto.getId()).toUri();
+        return ResponseEntity.created(uri).body(dto);
+    }
+
+}
