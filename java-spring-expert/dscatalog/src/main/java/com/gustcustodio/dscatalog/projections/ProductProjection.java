785b0c771d92f8947c5bb88040f1d605be17bf73
gustcustodio <gustcustodio@gmail.com>
2025-12-12 07:16:11 -0300

    feat: executando a consulta via método no JpaRepository

---
+++ java-spring-expert/dscatalog/src/main/java/com/gustcustodio/dscatalog/projections/ProductProjection.java
@@ -0,0 +1,8 @@
+package com.gustcustodio.dscatalog.projections;
+
+public interface ProductProjection {
+
+    Long getId();
+    String getName();
+
+}
--- java-spring-expert/dscatalog/src/main/java/com/gustcustodio/dscatalog/repositories/ProductRepository.java
+++ java-spring-expert/dscatalog/src/main/java/com/gustcustodio/dscatalog/repositories/ProductRepository.java
@@ -1,10 +1,36 @@
 package com.gustcustodio.dscatalog.repositories;
 
 import com.gustcustodio.dscatalog.entities.Product;
+import com.gustcustodio.dscatalog.projections.ProductProjection;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    @Query(nativeQuery = true, value = """
+             SELECT DISTINCT tb_product.id, tb_product.name\s
+             FROM tb_product\s
+             INNER JOIN tb_product_category\s
+             ON tb_product_category.product_id = tb_product.id\s
+             WHERE (:categoryIds IS NULL OR tb_product_category.category_id IN (:categoryIds))\s
+             AND (LOWER(tb_product.name) LIKE LOWER(CONCAT('%',:name,'%')))\s
+             ORDER BY tb_product.name\s
+            \s""", countQuery = """
+             SELECT COUNT(*) FROM (\s
+                     SELECT DISTINCT tb_product.id, tb_product.name\s
+                     FROM tb_product\s
+                     INNER JOIN tb_product_category\s
+                     ON tb_product_category.product_id = tb_product.id\s
+                     WHERE (:categoryIds IS NULL OR tb_product_category.category_id IN (:categoryIds))\s
+                     AND (LOWER(tb_product.name) LIKE LOWER(CONCAT('%',:name,'%')))\s
+                     ) AS tb_result\s
+            \s""")
+    Page<ProductProjection> searchProducts(List<Long> categoryIds, String name, Pageable pageable);
 
 }
