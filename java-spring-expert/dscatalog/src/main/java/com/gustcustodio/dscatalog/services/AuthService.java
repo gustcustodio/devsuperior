7db7593f9649d36a495cef718e7dcd822aedac7c
gustcustodio <gustcustodio@gmail.com>
2025-12-17 08:03:14 -0300

    feat: começando o primeiro passo da recuperação de senha

---
+++ java-spring-expert/dscatalog/src/main/java/com/gustcustodio/dscatalog/services/AuthService.java
@@ -0,0 +1,13 @@
+package com.gustcustodio.dscatalog.services;
+
+import com.gustcustodio.dscatalog.dtos.EmailDTO;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthService {
+
+    public void createRecoverToken(EmailDTO emailDTO) {
+
+    }
+
+}
