994c2e685d084a46b7e67ce9a1fd7b51754a0224
gustcustodio <gustcustodio@gmail.com>
2025-12-18 07:49:19 -0300

    feat: implementando a lógica de envio do token por email

---
+++ java-spring-expert/dscatalog/src/main/java/com/gustcustodio/dscatalog/repositories/PasswordRecoverRepository.java
@@ -0,0 +1,10 @@
+package com.gustcustodio.dscatalog.repositories;
+
+import com.gustcustodio.dscatalog.entities.PasswordRecover;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PasswordRecoverRepository extends JpaRepository<PasswordRecover, Long> {
+
+
+
+}
--- java-spring-expert/dscatalog/src/main/java/com/gustcustodio/dscatalog/services/AuthService.java
+++ java-spring-expert/dscatalog/src/main/java/com/gustcustodio/dscatalog/services/AuthService.java
@@ -1,13 +1,53 @@
 package com.gustcustodio.dscatalog.services;
 
 import com.gustcustodio.dscatalog.dtos.EmailDTO;
+import com.gustcustodio.dscatalog.entities.PasswordRecover;
+import com.gustcustodio.dscatalog.entities.User;
+import com.gustcustodio.dscatalog.repositories.PasswordRecoverRepository;
+import com.gustcustodio.dscatalog.repositories.UserRepository;
+import com.gustcustodio.dscatalog.services.exceptions.ResourceNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.Instant;
+import java.util.UUID;
 
 @Service
 public class AuthService {
 
-    public void createRecoverToken(EmailDTO emailDTO) {
+    @Value("${email.password-recover.token.minutes}")
+    private Long tokenMinutes;
 
+    @Value(("${email.password-recover.uri}"))
+    private String recoverUri;
+
+    @Autowired
+    private EmailService emailService;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private PasswordRecoverRepository passwordRecoverRepository;
+
+    @Transactional
+    public void createRecoverToken(EmailDTO emailDTO) {
+        User user = userRepository.findByEmail(emailDTO.getEmail());
+        if (user == null) throw new ResourceNotFoundException("Email não encontrado");
+
+        String token = UUID.randomUUID().toString();
+        PasswordRecover passwordRecover = new PasswordRecover();
+        passwordRecover.setEmail(emailDTO.getEmail());
+        passwordRecover.setToken(token);
+        passwordRecover.setExpirationTime(Instant.now().plusSeconds(tokenMinutes * 60L));
+        passwordRecoverRepository.save(passwordRecover);
+
+        String body =
+                "Acesse o link para definir uma nova senha\n\n" + recoverUri + token + ". Validade de " + tokenMinutes + " minutos.";
+
+        emailService.sendEmail(emailDTO.getEmail(), "Recuperação de senha", body);
     }
 
 }
