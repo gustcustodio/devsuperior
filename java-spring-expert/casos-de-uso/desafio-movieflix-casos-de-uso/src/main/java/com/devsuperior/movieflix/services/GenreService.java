4c83c392b68952c28b9848fbad90e64bfce26fea
gustcustodio <gustcustodio@gmail.com>
2025-12-20 07:14:59 -0300

    feat: implementando findAll do endpoint genres

---
+++ java-spring-expert/casos-de-uso/desafio-movieflix-casos-de-uso/src/main/java/com/devsuperior/movieflix/services/GenreService.java
@@ -0,0 +1,22 @@
+package com.devsuperior.movieflix.services;
+
+import com.devsuperior.movieflix.dto.GenreDTO;
+import com.devsuperior.movieflix.entities.Genre;
+import com.devsuperior.movieflix.repositories.GenreRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class GenreService {
+
+    @Autowired
+    private GenreRepository genreRepository;
+
+    public List<GenreDTO> findAll() {
+        List<Genre> list = genreRepository.findAll();
+        return list.stream().map(GenreDTO::new).toList();
+    }
+
+}
