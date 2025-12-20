7acffeef339649684bdb32bd9c29317163476a58
gustcustodio <gustcustodio@gmail.com>
2025-12-20 07:32:10 -0300

    feat: implementando findById do endpoint movies

---
+++ java-spring-expert/casos-de-uso/desafio-movieflix-casos-de-uso/src/main/java/com/devsuperior/movieflix/services/MovieService.java
@@ -0,0 +1,23 @@
+package com.devsuperior.movieflix.services;
+
+import com.devsuperior.movieflix.dto.MovieDetailsDTO;
+import com.devsuperior.movieflix.entities.Movie;
+import com.devsuperior.movieflix.repositories.MovieRepository;
+import com.devsuperior.movieflix.services.exceptions.ResourceNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class MovieService {
+
+    @Autowired
+    private MovieRepository movieRepository;
+
+    @Transactional(readOnly = true)
+    public MovieDetailsDTO findById(Long id) {
+        Movie movie = movieRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Resource not found"));
+        return new MovieDetailsDTO(movie);
+    }
+
+}
