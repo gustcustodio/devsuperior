a3893dc806579e52c1d510f93f49bd023a3570df
gustcustodio <gustcustodio@gmail.com>
2026-01-16 06:53:29 -0300

    feat: teste authenticated do UserService

--- java-spring-expert/cobertura-de-codigo/desafio-dsmovie-jacoco/src/test/java/com/devsuperior/dsmovie/services/UserServiceTests.java
+++ java-spring-expert/cobertura-de-codigo/desafio-dsmovie-jacoco/src/test/java/com/devsuperior/dsmovie/services/UserServiceTests.java
@@ -1,24 +1,61 @@
 package com.devsuperior.dsmovie.services;
 
+import com.devsuperior.dsmovie.entities.UserEntity;
+import com.devsuperior.dsmovie.repositories.UserRepository;
+import com.devsuperior.dsmovie.tests.UserFactory;
+import com.devsuperior.dsmovie.utils.CustomUserUtil;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.Optional;
 
 @ExtendWith(SpringExtension.class)
 @ContextConfiguration
 public class UserServiceTests {
 
 	@InjectMocks
-	private UserService service;
+	private UserService userService;
+
+	@Mock
+	private UserRepository userRepository;
+
+	@Mock
+	private CustomUserUtil customUserUtil;
+
+	private UserEntity user;
+	private String existingUsername, nonExistingUsername;
+
+	@BeforeEach
+	void setUp() throws Exception {
+		user = UserFactory.createUserEntity();
+		existingUsername = user.getUsername();
+		nonExistingUsername = "teste@gmail.com";
+
+		Mockito.when(userRepository.findByUsername(existingUsername)).thenReturn(Optional.of(user));
+		Mockito.when(userRepository.findByUsername(nonExistingUsername)).thenReturn(Optional.empty());
+	}
 
 	@Test
 	public void authenticatedShouldReturnUserEntityWhenUserExists() {
+		Mockito.when(customUserUtil.getLoggedUsername()).thenReturn(existingUsername);
+		UserEntity result = userService.authenticated();
+		Assertions.assertNotNull(result);
+		Assertions.assertEquals(existingUsername, result.getUsername());
+		Mockito.verify(userRepository, Mockito.times(1)).findByUsername(existingUsername);
 	}
 
 	@Test
 	public void authenticatedShouldThrowUsernameNotFoundExceptionWhenUserDoesNotExists() {
+		Mockito.doThrow(ClassCastException.class).when(customUserUtil).getLoggedUsername();
+		Assertions.assertThrows(UsernameNotFoundException.class, () -> userService.authenticated());
 	}
 
 	@Test
