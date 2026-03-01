46dfb1d35af5b539035df3de07b166bb66eff95a
gustcustodio <gustcustodio@gmail.com>
2026-03-01 12:51:51 -0300

    cria projeto dslearn

---
+++ java-spring-reference/modelo-de-dominio-complexo/dslearn/src/test/java/com/gustcustodio/dslearn/DslearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gustcustodio.dslearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DslearnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
