8b504c98dc875c38d11243a7af8dea776ece55a3
gustcustodio <gustcustodio@gmail.com>
2026-03-29 12:28:21 -0300

    feat: DELETE User

--- java-spring-reference/workshop-mongo/src/main/java/com/gustcustodio/workshop_mongo/services/UserService.java
+++ java-spring-reference/workshop-mongo/src/main/java/com/gustcustodio/workshop_mongo/services/UserService.java
@@ -40,6 +40,11 @@ public class UserService {
         return new UserDTO(entity);
     }
 
+    public void delete(String id) {
+        getEntityById(id);
+        userRepository.deleteById(id);
+    }
+
     private User getEntityById(String id) {
         Optional<User> result = userRepository.findById(id);
         return result.orElseThrow(() -> new ResourceNotFoundException("Objeto não encontrado"));
