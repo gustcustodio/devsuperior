1d927b3d80e3cbe18fb6028e9371f95f14e3e31f
gustcustodio <gustcustodio@gmail.com>
2026-05-02 06:25:32 -0300

    feat: teste busca de produto por nome

--- java-spring-expert/testes-de-integracao/dscommerce-restassured-sb3.4.4/src/test/java/com/devsuperior/dscommerce/controllers/ProductControllerRA.java
+++ java-spring-expert/testes-de-integracao/dscommerce-restassured-sb3.4.4/src/test/java/com/devsuperior/dscommerce/controllers/ProductControllerRA.java
@@ -13,18 +13,19 @@ import static org.hamcrest.Matchers.*;
 public class ProductControllerRA {
 
     private Long existingProductId, nonExistingProductId;
+    private String productName;
 
     @BeforeEach
     public void setUp() {
         baseURI = "http://localhost:8080";
+        existingProductId = 2L;
+        productName = "Macbook";
     }
 
     @Test
     public void findByIdShouldReturnProductWhenIdExists() {
-        existingProductId = 2L;
-
-        given().
-                get("products/{id}", existingProductId)
+        given()
+                .get("products/{id}", existingProductId)
         .then()
                 .statusCode(200)
                 .body("id", is(2))
@@ -33,6 +34,33 @@ public class ProductControllerRA {
                 .body("price", is(2190.0F))
                 .body("categories.id", hasItems(2, 3))
                 .body("categories.name", hasItems("Eletrônicos", "Computadores"));
+    }
+
+    @Test
+    public void findAllShouldReturnPagedProductsWhenProductNameParamIsEmpty() {
+        given()
+                .get("/products?page=0")
+        .then()
+                .statusCode(200)
+                .body("content.name", hasItems("Macbook Pro", "PC Gamer Tera"));
+    }
+
+    @Test
+    public void findAllShouldReturnPagedProductsWhenProductNameParamIsNotEmpty() {
+        given()
+                .get("/products?name={productName}", productName)
+        .then()
+                .statusCode(200)
+                .body("content.id[0]", is(3))
+                .body("content.name[0]", equalTo("Macbook Pro"));
+    }
+
+    @Test
+    public void findAllShouldReturnPagedProductsWithPriceGreaterThan2000() {
+        given()
+                .get("/products?size=25")
+        .then()
+                .body("content.findAll { it.price > 2000 }.name", hasItems("Smart TV", "PC Gamer Weed"));
     }
 
 }
