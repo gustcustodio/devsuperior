18a595f2d690dde2d71ee20419e4bf5858087477
gustcustodio <gustcustodio@gmail.com>
2026-05-03 13:38:06 -0300

    feat: desafio finalizado

--- java-spring-expert/testes-de-integracao/desafio/dsmovie-restassured/src/test/java/com/devsuperior/dsmovie/controllers/MovieControllerRA.java
+++ java-spring-expert/testes-de-integracao/desafio/dsmovie-restassured/src/test/java/com/devsuperior/dsmovie/controllers/MovieControllerRA.java
@@ -1,35 +1,137 @@
 package com.devsuperior.dsmovie.controllers;
 
+import com.devsuperior.dsmovie.tests.TokenUtil;
+import io.restassured.http.ContentType;
+import org.json.simple.JSONObject;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import static io.restassured.RestAssured.baseURI;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.*;
+
 public class MovieControllerRA {
-	
+
+	private Long existingMovieId, nonExistingMovieId;
+	private String movieTitle;
+	private String clientUsername, clientPassword, adminUsername, adminPassword;
+	private String adminToken, clientToken, invalidToken;
+
+	private Map<String, Object> postMovieInstance;
+
+	@BeforeEach
+	void setUp() throws Exception {
+		baseURI = "http://localhost:8080";
+
+		existingMovieId = 1L;
+		nonExistingMovieId = 1000L;
+
+		movieTitle = "Witcher";
+
+		clientUsername = "alex@gmail.com";
+		clientPassword = "123456";
+		adminUsername = "maria@gmail.com";
+		adminPassword = "123456";
+
+		clientToken = TokenUtil.obtainAccessToken(clientUsername, clientPassword);
+		adminToken = TokenUtil.obtainAccessToken(adminUsername, adminPassword);
+		invalidToken = adminToken + "xpto";
+
+		postMovieInstance = new HashMap<>();
+		postMovieInstance.put("title", "Gênio Indomável");
+		postMovieInstance.put("score", 4.1);
+		postMovieInstance.put("count", 3);
+		postMovieInstance.put("image", "https://image.tmdb.org/t/p/w600_and_h900_face/dTe3EpJGnfUeR5swLbWv61iRyFc.jpg");
+	}
+
 	@Test
 	public void findAllShouldReturnOkWhenMovieNoArgumentsGiven() {
+		given()
+				.get("/movies")
+		.then()
+				.statusCode(200)
+				.body("content.id", hasItems(1, 2))
+				.body("content.title", hasItems("The Witcher", "Venom: Tempo de Carnificina"));
 	}
 	
 	@Test
-	public void findAllShouldReturnPagedMoviesWhenMovieTitleParamIsNotEmpty() {		
+	public void findAllShouldReturnPagedMoviesWhenMovieTitleParamIsNotEmpty() {
+		given()
+				.get("/movies?title={movieTitle}", movieTitle)
+		.then()
+				.statusCode(200)
+				.body("content.id[0]", is(1))
+				.body("content.title[0]", is("The Witcher"))
+				.body("content.score[0]", is(4.5F))
+				.body("content.count[0]", is(2))
+				.body("content.image[0]", is("https://www.themoviedb.org/t/p/w533_and_h300_bestv2/jBJWaqoSCiARWtfV0GlqHrcdidd.jpg"));
 	}
 	
 	@Test
-	public void findByIdShouldReturnMovieWhenIdExists() {		
+	public void findByIdShouldReturnMovieWhenIdExists() {
+		given()
+				.get("/movies/{id}", existingMovieId)
+		.then()
+				.statusCode(200)
+				.body("title", is("The Witcher"))
+                .body("score", is(4.5F));
 	}
 	
 	@Test
-	public void findByIdShouldReturnNotFoundWhenIdDoesNotExist() {	
+	public void findByIdShouldReturnNotFoundWhenIdDoesNotExist() {
+		given()
+				.get("/movies/{id}", nonExistingMovieId)
+		.then()
+				.statusCode(404);
 	}
 	
 	@Test
 	public void insertShouldReturnUnprocessableEntityWhenAdminLoggedAndBlankTitle() {
+		postMovieInstance.put("title", "");
+		JSONObject newMovieJSON = new JSONObject(postMovieInstance);
+		given()
+				.header("Authorization", "Bearer " + adminToken)
+				.body(newMovieJSON)
+				.contentType(ContentType.JSON)
+				.accept(ContentType.JSON)
+		.when()
+				.post("/movies")
+		.then()
+				.statusCode(422)
+				.body("error", is("Dados inválidos"))
+				.body("errors.fieldName", hasItem("title"))
+				.body("errors.message", hasItems("Campo requerido", "Tamanho deve ser entre 5 e 80 caracteres"));
 	}
 	
 	@Test
 	public void insertShouldReturnForbiddenWhenClientLogged() {
+		JSONObject newMovieJSON = new JSONObject(postMovieInstance);
+		given()
+				.header("Authorization", "Bearer " + clientToken)
+				.body(newMovieJSON)
+				.contentType(ContentType.JSON)
+				.accept(ContentType.JSON)
+		.when()
+				.post("/movies")
+		.then()
+				.statusCode(403);
 	}
 	
 	@Test
 	public void insertShouldReturnUnauthorizedWhenInvalidToken() {
+		JSONObject newMovieJSON = new JSONObject(postMovieInstance);
+		given()
+				.header("Authorization", "Bearer " + invalidToken)
+				.body(newMovieJSON)
+				.contentType(ContentType.JSON)
+				.accept(ContentType.JSON)
+		.when()
+				.post("/movies")
+		.then()
+				.statusCode(401);
 	}
 	
 }
--- java-spring-expert/testes-de-integracao/desafio/dsmovie-restassured/src/test/java/com/devsuperior/dsmovie/controllers/ScoreControllerRA.java
+++ java-spring-expert/testes-de-integracao/desafio/dsmovie-restassured/src/test/java/com/devsuperior/dsmovie/controllers/ScoreControllerRA.java
@@ -1,19 +1,93 @@
 package com.devsuperior.dsmovie.controllers;
 
+import com.devsuperior.dsmovie.tests.TokenUtil;
+import io.restassured.http.ContentType;
+import org.json.simple.JSONObject;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static io.restassured.RestAssured.baseURI;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.*;
 
 public class ScoreControllerRA {
 
+    private Long existingMovieId, nonExistingMovieId;
+    private String adminUsername, adminPassword;
+    private String adminToken, clientToken;
+
+    private Map<String, Object> putScoreInstance;
+
+    @BeforeEach
+    void setUp() throws Exception {
+        baseURI = "http://localhost:8080";
+
+        existingMovieId = 1L;
+        nonExistingMovieId = 1000L;
+
+        adminUsername = "maria@gmail.com";
+        adminPassword = "123456";
+
+        adminToken = TokenUtil.obtainAccessToken(adminUsername, adminPassword);
+
+        putScoreInstance = new HashMap<>();
+        putScoreInstance.put("movieId", 1);
+        putScoreInstance.put("score", 5);
+    }
+
     @Test
     public void saveScoreShouldReturnNotFoundWhenMovieIdDoesNotExist() {
+        putScoreInstance.put("movieId", nonExistingMovieId);
+        JSONObject saveScoreJSON = new JSONObject(putScoreInstance);
+        given()
+                .header("Authorization", "Bearer " + adminToken)
+                .body(saveScoreJSON)
+                .contentType(ContentType.JSON)
+                .accept(ContentType.JSON)
+        .when()
+                .put("/scores")
+        .then()
+                .statusCode(404)
+                .body("error", is("Recurso não encontrado"));
     }
 
     @Test
     public void saveScoreShouldReturnUnprocessableEntityWhenMissingMovieId() {
+        putScoreInstance.put("movieId", "");
+        JSONObject saveScoreJSON = new JSONObject(putScoreInstance);
+        given()
+                .header("Authorization", "Bearer " + adminToken)
+                .body(saveScoreJSON)
+                .contentType(ContentType.JSON)
+                .accept(ContentType.JSON)
+        .when()
+                .put("/scores")
+        .then()
+                .statusCode(422)
+                .body("error", is("Dados inválidos"))
+                .body("errors.fieldName[0]", is("movieId"))
+                .body("errors.message[0]", is("Campo requerido"));
     }
 
     @Test
     public void saveScoreShouldReturnUnprocessableEntityWhenScoreIsLessThanZero() {
+        putScoreInstance.put("score", -1);
+        JSONObject saveScoreJSON = new JSONObject(putScoreInstance);
+        given()
+                .header("Authorization", "Bearer " + adminToken)
+                .body(saveScoreJSON)
+                .contentType(ContentType.JSON)
+                .accept(ContentType.JSON)
+        .when()
+                .put("/scores")
+        .then()
+                .statusCode(422)
+                .body("error", is("Dados inválidos"))
+                .body("errors.fieldName[0]", is("score"))
+                .body("errors.message[0]", is("Valor mínimo 0"));
     }
 
 }
